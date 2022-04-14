vijaymalav564/android_frameworks_base-1

base: Lockscreen blur level: code fixes
check if blur_radius < 1 before checking if it's <5

use existing statusbar settings observer to avoid too many settings
provider calls when triggering processArtwork (it's called also on
wake events when goint to lockscreen)

set the default system setting value to 2500,
because then we divide it for 100 to get the stock default value
that was 25 in MediaArtwork class.
Remember to set android:defaultValue to 2500 for the seekbar in
the Settings part of this commit.

Signed-off-by: Arghya Chanda <arghyac35@gmail.com>
Change-Id: Ieb71b835228e985b1b29b7c9698166dd47be7e95
Signed-off-by: Arghya Chanda <arghyac35@gmail.com>
Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
Signed-off-by: Sagarrokade006 <sagarrokade006@gmail.com>
Signed-off-by: SparXFusion <s2234nadar@gmail.com>

packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -4591,6 +4591,39 @@ public class StatusBar extends SystemUI implements DemoMode,
         return mDeviceInteractive;
     }
 
+    private SbSettingsObserver mSbSettingsObserver = new SbSettingsObserver(mHandler);
+    private class SbSettingsObserver extends ContentObserver {
+        SbSettingsObserver(Handler handler) {
+            super(handler);
+        }
+
+        void observe() {
+            ContentResolver resolver = mContext.getContentResolver();
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.LOCKSCREEN_MEDIA_BLUR),
+                    false, this, UserHandle.USER_ALL);
+        }
+
+        @Override
+        public void onChange(boolean selfChange, Uri uri) {
+            super.onChange(selfChange, uri);
+            if (uri.equals(Settings.Secure.getUriFor(
+                    Settings.System.LOCKSCREEN_MEDIA_BLUR))) {
+                setLockScreenMediaBlurLevel();
+            }
+        }
+
+        public void update() {
+            setLockScreenMediaBlurLevel();
+        }
+    }
+
+    private void setLockScreenMediaBlurLevel() {
+        if (mMediaManager != null) {
+            mMediaManager.setLockScreenMediaBlurLevel();
+        }
+    }
+
     private final BroadcastReceiver mBannerActionBroadcastReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {

404-Mess/android_frameworks_base-2

packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -1013,26 +1013,29 @@ public class StatusBar extends SystemUI implements
 
         void observe() {
             ContentResolver resolver = mContext.getContentResolver();
-            /*resolver.registerContentObserver(Settings.System.getUriFor(
-                    Settings.System.XXX),
-                    false, this, UserHandle.USER_ALL);*/
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.LOCKSCREEN_MEDIA_BLUR),
+                    false, this, UserHandle.USER_ALL);
         }
 
         @Override
         public void onChange(boolean selfChange, Uri uri) {
-            /*if (uri.equals(Settings.System.getUriFor(
-                    Settings.System.XXX))) {
-                doXXX();
-            }*/
+		if (uri.equals(Settings.System.getUriFor(
+                    Settings.System.LOCKSCREEN_MEDIA_BLUR))) {
+                setLockScreenMediaBlurLevel();
+            }
         }
 
         public void update() {
-            //doXXX();
+            setLockScreenMediaBlurLevel();
         }
     }
 
-    /*private void doXXX() {
-    }*/
+    private void setLockScreenMediaBlurLevel() {
+        if (mMediaManager != null) {
+            mMediaManager.setLockScreenMediaBlurLevel();
+        }
+    }
         // Set up the initial icon state
         int numIcons = result.mIcons.size();
         for (int i = 0; i < numIcons; i++) {
@@ -1104,7 +1107,8 @@ public class StatusBar extends SystemUI implements
         mFalsingManager.addFalsingBeliefListener(mFalsingBeliefListener);
 
         mPluginManager.addPluginListener(
-                new PluginListener<OverlayPlugin>() {
+
+           new PluginListener<OverlayPlugin>() {
                     private final ArraySet<OverlayPlugin> mOverlays = new ArraySet<>();
 
                     @Override